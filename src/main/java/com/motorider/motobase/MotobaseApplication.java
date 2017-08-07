4aaf655c496d4868e0d36daca5218aeabc6254e7
Julian Barragan <julbarg@gmail.com>
2017-08-07 15:55:08 -0500

    Init Commit

---
+++ src/main/java/com/motorider/motobase/MotobaseApplication.java
@@ -0,0 +1,12 @@
+package com.motorider.motobase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotobaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotobaseApplication.class, args);
+	}
+}
